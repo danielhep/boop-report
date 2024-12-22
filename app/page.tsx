2cb43293a33255885e31c5a8973254868ef86a1a
danielhep <dh@danielheppner.com>
2024-12-21 23:20:21 -0800

    fix hardcoded check if data exists

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+"use client";
 import MainCards from "@/components/MainCards";
 import TopSection from "@/components/TopSection";
 import NoDataMessage from "@/components/NoDataMessage";
+import { useOrcaStore } from "@/lib/store/orcaStoreProvider";
 
 // TODO: Replace this with actual data check
-const hasOrcaData = false;
 
 export default function Home() {
+	const rawData = useOrcaStore((state) => state.rawData);
+  const hasOrcaData = rawData && rawData?.length > 0;
 	return (
 		<div className="flex flex-col">
 			<TopSection />
